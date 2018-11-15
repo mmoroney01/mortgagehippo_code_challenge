2f6e3c823196508fddfec3e2fcfdfa3e9c50c110
Michael Moroney <mmoroney06@gmail.com>
2018-11-15 12:04:45 -0600

    user can deposit coin and see all transactions

---
+++ app/controllers/transactions_controller.rb
@@ -0,0 +1,39 @@
+class TransactionsController < ApplicationController
+  def see_all
+  	@transactions = Transaction.all
+
+  	p @transactions
+
+  	render 'transactions/index'
+  end
+
+  def create
+  	@coin = Coin.find_by(:name => params[:name], :value => params[:value].to_i)
+
+  	@coin.update(deposited?: true)
+
+  	Transaction.create(coin_id: @coin.id, apiuser_id: "1")
+
+  	respond_to do |f|
+      f.html { redirect_to root_path }
+      f.js
+    end
+  end
+
+  def see_all_transactions_one_user
+
+  	p params
+
+
+
+  	@apiuser = Apiuser.find_by(api_key: params[:api_key])
+
+  	p @apiuser
+
+  	# @transactions = Transaction.find_by(apiuser_id: @apiuser.id)
+
+  	# p @transactions
+
+  	render 'transactions/index'
+  end
+end
--- db/migrate/20181115171704_create_coins.rb
+++ db/migrate/20181115171704_create_coins.rb
@@ -1,8 +1,9 @@
 class CreateCoins < ActiveRecord::Migration[5.1]
   def change
     create_table :coins do |t|
-      t.string :name
-      t.integer :value
+      t.string :name, null: false
+      t.integer :value, null: false
+      t.boolean :deposited?, default: false
 
       t.timestamps
     end
